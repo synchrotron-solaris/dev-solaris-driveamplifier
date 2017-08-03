23187acfbcb0fabfe861c010f8e1b18c916e945d
stanislaw47 <zefg>
2017-08-03 11:00:41 +0200

    #1 minor whitespace fix

--- drive_amplifier_ds/__init__.py
+++ drive_amplifier_ds/__init__.py
@@ -8,3 +8,6 @@ This package contains Drive Amplifier Device Class based on facadedevice library
 __all__ = ['drive_amplifier', 'version']
 __author__ = "Stanislaw Cabala"
 __author_email__ = "stanislaw.cabala@gmail.com"
+
+
+
